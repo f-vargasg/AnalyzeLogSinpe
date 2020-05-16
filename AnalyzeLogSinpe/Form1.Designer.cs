d2e5a279d87ffa7b0c56332a1aaf7cd88989eeb9
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2020-05-16 12:42:06 -0600

    Add project files.

---
+++ AnalyzeLogSinpe/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace AnalyzeLogSinpe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tLstripButRefresh = new System.Windows.Forms.ToolStripButton();
+            this.tLstripButAdd = new System.Windows.Forms.ToolStripButton();
+            this.tLstripButDel = new System.Windows.Forms.ToolStripButton();
+            this.tLstripButVer = new System.Windows.Forms.ToolStripButton();
+            this.tLstripButIndexar = new System.Windows.Forms.ToolStripButton();
+            this.tLstripButEnv = new System.Windows.Forms.ToolStripButton();
+            this.tLstripButUpDoc = new System.Windows.Forms.ToolStripButton();
+            this.tLstripButSeal = new System.Windows.Forms.ToolStripButton();
+            this.tLstripButGetNumber = new System.Windows.Forms.ToolStripButton();
+            this.tLstripButSalir = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tLstripButRefresh,
+            this.tLstripButAdd,
+            this.tLstripButDel,
+            this.tLstripButVer,
+            this.tLstripButIndexar,
+            this.tLstripButEnv,
+            this.tLstripButUpDoc,
+            this.tLstripButSeal,
+            this.tLstripButGetNumber,
+            this.tLstripButSalir});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1200, 27);
+            this.toolStrip1.TabIndex = 11;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tLstripButRefresh
+            // 
+            this.tLstripButRefresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tLstripButRefresh.Image = ((System.Drawing.Image)(resources.GetObject("tLstripButRefresh.Image")));
+            this.tLstripButRefresh.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tLstripButRefresh.Name = "tLstripButRefresh";
+            this.tLstripButRefresh.Size = new System.Drawing.Size(24, 24);
+            this.tLstripButRefresh.Text = "toolStripButton3";
+            this.tLstripButRefresh.ToolTipText = "Refrescar";
+            // 
+            // tLstripButAdd
+            // 
+            this.tLstripButAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tLstripButAdd.Image = ((System.Drawing.Image)(resources.GetObject("tLstripButAdd.Image")));
+            this.tLstripButAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tLstripButAdd.Name = "tLstripButAdd";
+            this.tLstripButAdd.Size = new System.Drawing.Size(24, 24);
+            this.tLstripButAdd.Text = "tStrAdd";
+            this.tLstripButAdd.ToolTipText = "Añadir";
+            // 
+            // tLstripButDel
+            // 
+            this.tLstripButDel.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tLstripButDel.Image = ((System.Drawing.Image)(resources.GetObject("tLstripButDel.Image")));
+            this.tLstripButDel.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tLstripButDel.Name = "tLstripButDel";
+            this.tLstripButDel.Size = new System.Drawing.Size(24, 24);
+            this.tLstripButDel.Text = "toolStripButton1";
+            this.tLstripButDel.ToolTipText = "Delete";
+            // 
+            // tLstripButVer
+            // 
+            this.tLstripButVer.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tLstripButVer.Image = ((System.Drawing.Image)(resources.GetObject("tLstripButVer.Image")));
+            this.tLstripButVer.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tLstripButVer.Name = "tLstripButVer";
+            this.tLstripButVer.Size = new System.Drawing.Size(24, 24);
+            this.tLstripButVer.Text = "toolStripButton1";
+            this.tLstripButVer.ToolTipText = "Ver Documento";
+            // 
+            // tLstripButIndexar
+            // 
+            this.tLstripButIndexar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tLstripButIndexar.Image = ((System.Drawing.Image)(resources.GetObject("tLstripButIndexar.Image")));
+            this.tLstripButIndexar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tLstripButIndexar.Name = "tLstripButIndexar";
+            this.tLstripButIndexar.Size = new System.Drawing.Size(24, 24);
+            this.tLstripButIndexar.Text = "toolStripButton2";
+            this.tLstripButIndexar.ToolTipText = "Indexar";
+            // 
+            // tLstripButEnv
+            // 
+            this.tLstripButEnv.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tLstripButEnv.Image = ((System.Drawing.Image)(resources.GetObject("tLstripButEnv.Image")));
+            this.tLstripButEnv.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tLstripButEnv.Name = "tLstripButEnv";
+            this.tLstripButEnv.Size = new System.Drawing.Size(24, 24);
+            this.tLstripButEnv.Text = "toolStripButton1";
+            this.tLstripButEnv.ToolTipText = "Enviar";
+            // 
+            // tLstripButUpDoc
+            // 
+            this.tLstripButUpDoc.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tLstripButUpDoc.Image = ((System.Drawing.Image)(resources.GetObject("tLstripButUpDoc.Image")));
+            this.tLstripButUpDoc.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tLstripButUpDoc.Name = "tLstripButUpDoc";
+            this.tLstripButUpDoc.Size = new System.Drawing.Size(24, 24);
+            this.tLstripButUpDoc.Text = "toolStripButton1";
+            this.tLstripButUpDoc.ToolTipText = "Actualizar documento";
+            // 
+            // tLstripButSeal
+            // 
+            this.tLstripButSeal.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tLstripButSeal.Image = ((System.Drawing.Image)(resources.GetObject("tLstripButSeal.Image")));
+            this.tLstripButSeal.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tLstripButSeal.Name = "tLstripButSeal";
+            this.tLstripButSeal.Size = new System.Drawing.Size(24, 24);
+            this.tLstripButSeal.Text = "toolStripButton1";
+            this.tLstripButSeal.ToolTipText = "Sellar documento";
+            // 
+            // tLstripButGetNumber
+            // 
+            this.tLstripButGetNumber.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tLstripButGetNumber.Image = ((System.Drawing.Image)(resources.GetObject("tLstripButGetNumber.Image")));
+            this.tLstripButGetNumber.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tLstripButGetNumber.Name = "tLstripButGetNumber";
+            this.tLstripButGetNumber.Size = new System.Drawing.Size(24, 24);
+            this.tLstripButGetNumber.Text = "toolStripButton1";
+            this.tLstripButGetNumber.ToolTipText = "Obtener numero";
+            // 
+            // tLstripButSalir
+            // 
+            this.tLstripButSalir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tLstripButSalir.Image = ((System.Drawing.Image)(resources.GetObject("tLstripButSalir.Image")));
+            this.tLstripButSalir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tLstripButSalir.Name = "tLstripButSalir";
+            this.tLstripButSalir.Size = new System.Drawing.Size(24, 24);
+            this.tLstripButSalir.Text = "toolStripButton1";
+            this.tLstripButSalir.ToolTipText = "Salir";
+            this.tLstripButSalir.Click += new System.EventHandler(this.tLstripButSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.toolStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tLstripButRefresh;
+        private System.Windows.Forms.ToolStripButton tLstripButAdd;
+        private System.Windows.Forms.ToolStripButton tLstripButDel;
+        private System.Windows.Forms.ToolStripButton tLstripButVer;
+        private System.Windows.Forms.ToolStripButton tLstripButIndexar;
+        private System.Windows.Forms.ToolStripButton tLstripButEnv;
+        private System.Windows.Forms.ToolStripButton tLstripButUpDoc;
+        private System.Windows.Forms.ToolStripButton tLstripButSeal;
+        private System.Windows.Forms.ToolStripButton tLstripButGetNumber;
+        private System.Windows.Forms.ToolStripButton tLstripButSalir;
+    }
+}
+
