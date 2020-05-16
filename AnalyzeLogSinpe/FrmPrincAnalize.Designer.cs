01ae9537946a114f9c0b967001c885dad0150e43
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2020-05-16 12:52:07 -0600

    Se cambio el nombre de principal de la forma

--- AnalyzeLogSinpe/FrmPrincAnalize.Designer.cs
+++ AnalyzeLogSinpe/FrmPrincAnalize.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AnalyzeLogSinpe
 {
-    partial class Form1
+    partial class FrmPrincAnalize
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincAnalize));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.tLstripButRefresh = new System.Windows.Forms.ToolStripButton();
             this.tLstripButAdd = new System.Windows.Forms.ToolStripButton();
--- AnalyzeLogSinpe/FrmPrincAnalize.cs
+++ AnalyzeLogSinpe/FrmPrincAnalize.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AnalyzeLogSinpe
 {
-    public partial class Form1 : Form
+    public partial class FrmPrincAnalize : Form
     {
-        public Form1()
+        public FrmPrincAnalize()
         {
             InitializeComponent();
         }
